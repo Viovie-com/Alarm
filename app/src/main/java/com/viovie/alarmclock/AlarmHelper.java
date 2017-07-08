082a04a5b75559a8cc6d6874fe44f5c891804fdd
曾智聖 <chr4337@gmail.com>
2017-07-08 15:16:16 +0800

    fix: alarm manager update not working

--- app/src/main/java/com/viovie/alarmclock/AlarmHelper.java
+++ app/src/main/java/com/viovie/alarmclock/AlarmHelper.java
@@ -8,14 +8,10 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 
 public class AlarmHelper {
-    public static int getRequestCode(int position) {
-        return 1000 + position;
-    }
-
     public static void create(@NonNull Context context, int position, AlarmItem item) {
         Intent intent = buildReceiverIntent(context, position, item);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                context, getRequestCode(position), intent, PendingIntent.FLAG_ONE_SHOT);
+                context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -28,7 +24,7 @@ public class AlarmHelper {
     public static void update(@NonNull Context context, int position, AlarmItem item) {
         Intent intent = buildReceiverIntent(context, position, item);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                context, getRequestCode(position), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -41,7 +37,7 @@ public class AlarmHelper {
     public static void delete(@NonNull Context context, int position) {
         Intent intent = buildReceiverIntent(context, position, null);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                context, getRequestCode(position), intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         manager.cancel(pendingIntent);
@@ -49,6 +45,7 @@ public class AlarmHelper {
 
     protected static Intent buildReceiverIntent(@NonNull Context context, int position, AlarmItem item) {
         Intent intent = new Intent(context, AlarmReceiver.class);
+        intent.setAction("alarm" + position); // set same intent
         intent.putExtra(AlarmReceiver.PARAM_POSITION, position);
         if (item != null) {
             intent.putExtra(AlarmReceiver.PARAM_ALARM_ITEM, item);
